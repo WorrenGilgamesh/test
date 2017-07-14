176ddf1552005db8d2464d86a9810176327ea0ea
Victor Osornio <victor.osornio@emqu.net>
2017-07-14 17:45:32 -0500

    asda

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+	Route::get('/', function () {
+	    return view('welcome');
+	});
+
+	Route::get('/Usuario',['uses'=>'UsuarioController@index']); 
+    Route::post('/buscarUsuario',['uses'=>'UsuarioController@search']);
+    Route::put('/Usuario',['uses'=>'UsuarioController@update']);
+    Route::delete('/Usuario/{id}',['uses'=>'UsuarioController@destroy']);
