8e6dd2f2eb306d097aaac7fbe69bd2a432e5502d
uboat46 <uboat46@hotmail.com>
2017-07-14 20:12:11 -0500

    header

--- resources/views/app/controllers/headerCtrl.js
+++ resources/views/app/controllers/headerCtrl.js
@@ -2,7 +2,13 @@
     'use strict';
 
     var app = angular.module('app');
-    app.controller('headerCtrl',function($scope){
-        
+    app.component('headerComponent',{
+        templateUrl: 'app/components/header.html',
+        controller: headerCtrl,
+        controllerAs: 'vm'
     });
+    function headerCtrl($scope){
+        
+    };
+    
 })();
